451045ac8fcaa7050bf0ee88ccc2246d4dd9cae6
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2025-02-18 10:30:59 -0300

    feat: webserver

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := "Gustavo"
+	age := "24"
+	fmt.Fprintf(w, "Hello! I'm %s, I'm %s\n", name, age)
+}
